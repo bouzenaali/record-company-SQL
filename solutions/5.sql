3a02347660313d4426c7443f5abb541469cebee9
bouzenaali <Alibouzena16@gmail.com>
2024-01-07 23:01:17 +0100

    feat: edit the fifth question

---
+++ solutions/5.sql
@@ -0,0 +1,4 @@
+SELECT albums.name AS Name, release_year AS 'Release Year', SUM(songs.length) AS 'duration'  FROM `albums`
+JOIN songs ON songs.album_id = albums.id
+GROUP BY songs.album_id
+ORDER BY duration DESC LIMIT 1;
